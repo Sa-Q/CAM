fdc626c7eba6d7c189929cbb53e23529ca1e5562
Sa-Q <sagay@lunarflake.com>
2024-07-15 11:54:07 +0900

    OLED ディスプレイ表示追加

--- cam.py
+++ cam.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/home/pi/V_CAM/bin/python3
 
 """
 ###########################################################################
@@ -6,7 +6,8 @@
 
 #Filename      :cam.py
 
-#Update        :2024/07/06
+#Update        :2024/07/15
+2024/07/15  Ver. α 0.02 OLED に文字、プログレスバー表示
 2024/07/06  Ver. α 0.01 SW ブッシュ で LED 点灯
 ############################################################################
 """
@@ -27,6 +28,37 @@ from gpiozero import LED
 # LED 初期設定
 LEDPIN1 = LED(17)
 LEDPIN2 = LED(27)
+
+
+# OLED ディスプレイ用拡張
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
+
+# OLED ディスプレイ用初期設定
+# Raspberry Pi 4以降の場合、port=1を指定
+serial = i2c(port=1, address=0x3C)
+# その他の初期化パラメータを設定（必要に応じて）
+device = ssd1306(serial, width=128, height=32) # 必要に応じ64
+
+from PIL import  ImageFont
+def oled_text(text,size):
+    # フォントを指定
+    font = ImageFont.truetype("fonts-japanese-gothic.ttf", size)
+    with canvas(device) as draw:
+        draw.text((0, 0), text, font=font, fill="white")
+
+def oled_square(x1,y1,x2,y2,color):
+    if color == 0:
+        #矩形の描画:
+        with canvas(device) as draw:
+            draw.rectangle((x1,y1,x2,y2), outline="white",fill="black")
+    if color == 1:
+        #矩形の描画:
+        with canvas(device) as draw:
+            draw.rectangle((x1,y1,x2,y2), outline="white",fill="white")
+
+
 
 #print message at the begining ---custom function
 def print_message():
@@ -70,9 +102,15 @@ def main():
         if sw_ =='on':
             # LED１点灯
             LEDPIN1.on()
+            # OLED ディスプレイ文字出力
+            text = "静止画撮影！"
+            oled_text(text,22)
             time.sleep(1)
             # LED１消灯
             LEDPIN1.off()
+            # １秒待って OLED ディスプレイクリア
+            time.sleep(1)
+            device.clear()
 #            break
 
         # スイッチ２状態取得
@@ -81,7 +119,13 @@ def main():
         if sw_ =='on':
             # LED２点灯
             LEDPIN2.on()
-            time.sleep(1)
+            # プログレスバー描画
+            for x in range(0,127,1):
+                oled_square(0, 10, x, 21,1)
+                time.sleep(0.1)
+#            time.sleep( rec_time )
+            # OLED ディスプレイクリア
+            device.clear()
             # LED２消灯
             LEDPIN2.off()
 #            break
